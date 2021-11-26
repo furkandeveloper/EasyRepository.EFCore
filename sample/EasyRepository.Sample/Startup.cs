e77b2fcf964190f1d2e82b893221d5be3190835d
Furkan Güngör <furkan.dvlp@gmail.com>
2021-11-26 12:48:17 +0300

    🔨 Implement Startup configuration for EasyRepository

--- sample/EasyRepository.Sample/Startup.cs
+++ sample/EasyRepository.Sample/Startup.cs
@@ -1,3 +1,5 @@
+using EasyRepository.EFCore.Generic;
+using EasyRepository.Sample.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +18,7 @@ namespace EasyRepository.Sample
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.ApplyEasyRepository<SampleDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
