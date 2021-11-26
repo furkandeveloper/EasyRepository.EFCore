4592f3128ee3ef9225cf939771d9b845f8912679
Furkan Güngör <furkan.dvlp@gmail.com>
2021-11-26 12:56:07 +0300

    🔨 Configure database connection

--- sample/EasyRepository.Sample/Startup.cs
+++ sample/EasyRepository.Sample/Startup.cs
@@ -3,6 +3,8 @@ using EasyRepository.Sample.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -14,12 +16,23 @@ namespace EasyRepository.Sample
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
             services.ApplyEasyRepository<SampleDbContext>();
+
+            services.AddDbContext<SampleDbContext>(options =>
+            {
+                options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
+            }, ServiceLifetime.Transient);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
