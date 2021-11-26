9ba94d07f6f519e352921449cd941f54b47d0845
Furkan Güngör <furkan.dvlp@gmail.com>
2021-11-26 12:37:17 +0300

    🔨 Add sample db context

---
+++ sample/EasyRepository.Sample/Context/SampleDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyRepository.Sample.Context
+{
+    /// <summary>
+    /// Sample Database Context
+    /// </summary>
+    public class SampleDbContext : DbContext
+    {
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="options">
+        /// The options to be used by a Microsoft.EntityFrameworkCore.DbContext. You normally
+        ///     override Microsoft.EntityFrameworkCore.DbContext.OnConfiguring(Microsoft.EntityFrameworkCore.DbContextOptionsBuilder)
+        ///     or use a Microsoft.EntityFrameworkCore.DbContextOptionsBuilder to create instances
+        ///     of this class and it is not designed to be directly constructed in your application
+        ///     code.
+        /// </param>
+        public SampleDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected SampleDbContext()
+        {
+        }
+    }
+}
