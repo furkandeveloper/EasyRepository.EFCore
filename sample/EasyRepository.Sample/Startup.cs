26d9b65e36ea5b0b029d99acf19b4543ad348714
Furkan Güngör <furkan.dvlp@gmail.com>
2021-11-26 12:48:56 +0300

    🔨 Add Add Controller With Wiews

--- sample/EasyRepository.Sample/Startup.cs
+++ sample/EasyRepository.Sample/Startup.cs
@@ -18,6 +18,7 @@ namespace EasyRepository.Sample
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
             services.ApplyEasyRepository<SampleDbContext>();
         }
 
