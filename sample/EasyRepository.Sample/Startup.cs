f46a9e6d7878a64e4ec6da4465c1b4591d70d623
Furkan Güngör <furkan.dvlp@gmail.com>
2021-11-26 13:05:39 +0300

    🔨 Implement swagger for sample project

--- sample/EasyRepository.Sample/Startup.cs
+++ sample/EasyRepository.Sample/Startup.cs
@@ -1,5 +1,7 @@
+using AutoFilterer.Swagger;
 using EasyRepository.EFCore.Generic;
 using EasyRepository.Sample.Context;
+using MarkdownDocumenting.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -9,7 +11,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace EasyRepository.Sample
@@ -33,6 +37,31 @@ namespace EasyRepository.Sample
             {
                 options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
             }, ServiceLifetime.Transient);
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("EasyRepository", new Microsoft.OpenApi.Models.OpenApiInfo()
+                {
+                    Title = "Easy Repository",
+                    Version = "1.0.0",
+                    Description = "This repo, provides implement generic repository pattern on ef core",
+                    Contact = new Microsoft.OpenApi.Models.OpenApiContact()
+                    {
+                        Email = "furkan.dvlp@gmail.com",
+                        Url = new Uri("https://github.com/furkandeveloper/EasyRepository.EFCore")
+                    }
+                });
+                options.UseAutoFiltererParameters();
+                var docFile = $"{Assembly.GetEntryAssembly().GetName().Name}.xml";
+                var filePath = Path.Combine(AppContext.BaseDirectory, docFile);
+
+                if (File.Exists((filePath)))
+                {
+                    options.IncludeXmlComments(filePath);
+                }
+                options.DescribeAllParametersInCamelCase();
+            });
+            services.AddDocumentation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,7 +73,18 @@ namespace EasyRepository.Sample
             }
 
             app.UseRouting();
+            app.UseSwagger();
 
+            app.UseSwaggerUI(options =>
+            {
+                options.EnableDeepLinking();
+                options.ShowExtensions();
+                options.DisplayRequestDuration();
+                options.DocExpansion(Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.None);
+                options.RoutePrefix = "api-docs";
+                options.SwaggerEndpoint("/swagger/EasyProfiler/swagger.json", "EasyProfilerSwagger");
+            });
+            app.UseDocumentation(opts => this.Configuration.Bind("DocumentationOptions", opts));
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
