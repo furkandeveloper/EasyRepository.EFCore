c323dce83723e18252068391a3a06d616222e701
Furkan Güngör <furkan.dvlp@gmail.com>
2021-11-27 21:33:05 +0300

    🔨 Add Author Request Data Transfer Object

---
+++ sample/EasyRepository.Sample/Dtos/Request/AuthorRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyRepository.Sample.Dtos.Request
+{
+    public class AuthorRequestDto
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+    }
+}
