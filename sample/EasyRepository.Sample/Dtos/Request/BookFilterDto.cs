bf052e1e3308a446ea8fdddff317d5ef8b1f7960
Furkan Güngör <furkan.dvlp@gmail.com>
2021-11-27 21:33:19 +0300

    🔨 Add Book Filter Data Transfer Object

---
+++ sample/EasyRepository.Sample/Dtos/Request/BookFilterDto.cs
@@ -0,0 +1,29 @@
+﻿using AutoFilterer.Attributes;
+using AutoFilterer.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyRepository.Sample.Dtos.Request
+{
+    [PossibleSortings("Title", "CreationDate", "ModificationDate", "DeletionDate")]
+    public class BookFilterDto : PaginationFilterBase
+    {
+        public BookFilterDto()
+        {
+            this.Sort = "Title";
+            this.SortBy = AutoFilterer.Enums.Sorting.Descending;
+        }
+
+        [ToLowerContainsComparison]
+        public string Title { get; set; }
+
+        public Range<int> TotalPage { get; set; }
+
+        public Range<DateTime> CreationDate { get; set; }
+
+        public Range<DateTime> ModificationDate { get; set; }
+        public Range<DateTime> DeletionDate { get; set; }
+    }
+}
