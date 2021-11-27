b068a4463be5ebc9f6941f3aa90523823ef5f074
Furkan Güngör <furkan.dvlp@gmail.com>
2021-11-27 21:32:51 +0300

    🔨 Add Author Filter Data Transfer Object

---
+++ sample/EasyRepository.Sample/Dtos/Request/AuthorFilterDto.cs
@@ -0,0 +1,30 @@
+﻿using AutoFilterer.Attributes;
+using AutoFilterer.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyRepository.Sample.Dtos.Request
+{
+    [PossibleSortings("Name", "Surname","CreationDate", "ModificationDate", "DeletionDate")]
+    public class AuthorFilterDto : PaginationFilterBase
+    {
+        public AuthorFilterDto()
+        {
+            this.Sort = "Name";
+            this.SortBy = AutoFilterer.Enums.Sorting.Descending;
+        }
+
+        [ToLowerContainsComparison]
+        public string Name { get; set; }
+
+        [ToLowerContainsComparison]
+        public string Surname { get; set; }
+
+        public Range<DateTime> CreationDate { get; set; }
+
+        public Range<DateTime> ModificationDate { get; set; }
+        public Range<DateTime> DeletionDate { get; set; }
+    }
+}
