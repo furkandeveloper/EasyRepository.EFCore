2c6858e2e40beb071aed05460cacb30a2519c105
furkandeveloper <furkan.dvlp@gmail.com>
2022-07-02 23:44:23 +0300

    🔨 Add SpecificationBuilder.cs to the Ardalis.Specification project

---
+++ src/EasyRepository.EFCore.Ardalis.Specification/SpecificationBuilder.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using Ardalis.Specification;
+using Ardalis.Specification.EntityFrameworkCore;
+
+namespace EasyRepository.EFCore.Ardalis.Specification;
+
+public static class SpecificationBuilder
+{
+    public static IQueryable<TEntity> Build<TEntity>(IQueryable<TEntity> entity, ISpecification<TEntity> specification) where TEntity : class, new()
+    {
+        return SpecificationEvaluator.Default.GetQuery(entity, specification);
+    }
+}
