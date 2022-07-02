0184fec37d81e3421c11c592b4452395b7ee157f
furkandeveloper <furkan.dvlp@gmail.com>
2022-07-02 23:55:50 +0300

    🔨 Add Ardalis.Specification sample code block

---
+++ sample/EasyRepository.Sample/Specs/AuthorByNameSpec.cs
@@ -0,0 +1,17 @@
+﻿using Ardalis.Specification;
+using EasyRepository.Sample.Entities;
+
+namespace EasyRepository.Sample.Specs
+{
+    /// <summary>
+    /// Author By Name specification
+    /// </summary>
+    public sealed class AuthorByNameSpec : Specification<Author>
+    {
+        /// <inheritdoc />
+        public AuthorByNameSpec(string name)
+        {
+            Query.Where(c => c.Name == name);
+        }
+    }
+}
