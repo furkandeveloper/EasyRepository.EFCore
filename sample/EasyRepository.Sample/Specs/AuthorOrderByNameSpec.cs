e2ccfc136e9ef14990b2fbd11654f68b0a12f2ea
furkandeveloper <furkan.dvlp@gmail.com>
2022-07-03 14:38:12 +0300

    🔨 Add order by example

---
+++ sample/EasyRepository.Sample/Specs/AuthorOrderByNameSpec.cs
@@ -0,0 +1,36 @@
+﻿using Ardalis.Specification;
+using EasyRepository.Sample.Entities;
+
+namespace EasyRepository.Sample.Specs
+{
+    /// <summary>
+    /// Order by author name specification
+    /// </summary>
+    public sealed class AuthorOrderByNameSpec : Specification<Author>
+    {
+        /// <inheritdoc />
+        public AuthorOrderByNameSpec(string name)
+        {
+            Query.ApplyBaseRules().ApplyByName(name).OrderBy(o => o.Name);
+        }
+    }
+
+    public static class AuthorSpecification
+    {
+        public static ISpecificationBuilder<Author> ApplyBaseRules(
+            this ISpecificationBuilder<Author> specificationBuilder)
+        {
+            specificationBuilder.Include(x => x.Books);
+
+            return specificationBuilder;
+        }
+        
+        public static ISpecificationBuilder<Author> ApplyByName(
+            this ISpecificationBuilder<Author> specificationBuilder, string name)
+        {
+            specificationBuilder.Where(a => a.Name.Contains(name));
+
+            return specificationBuilder;
+        }
+    }
+}
